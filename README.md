009762ed67aa23cf1bd6581e4b5f2413b6687b2c
Brent Swain <bmswain@gmail.com>
2014-04-30 20:18:40 -0600

    Add Working AngularJS Calculator

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-simple-calculator
-=================
+## Simple Calculator - AngularJS
 
-Simple Calculator AngularJS app
+- Performs 4 arithmetic operations
+- Equals sign button evaluates and displays the result of the inputed numbers and operation
+- Clear button resets calculator to 0 value
---
+++ app/js/app.js
@@ -0,0 +1,50 @@
+
+var app = angular.module("simpleCalculator", []);
+
+app.controller("CalcController", function($scope) {
+
+  $scope.equalsSign = "=";
+  $scope.clear = "C";
+  $scope.clearDisplay = true;
+  $scope.output = "0";
+  $scope.tempNumberA = 0;
+  $scope.tempNumberB = 0;
+  $scope.digits = ["7","8","9","4","5","6","1","2","3","0"];
+  $scope.operators = ["/","*","-","+"];
+
+  $scope.clearClick = function () {
+    $scope.clearDisplay = true;
+    $scope.output = "0";
+    $scope.tempNumberA = 0;
+    $scope.tempNumberB = 0;
+    $scope.currentOperator = null;
+  };
+
+  $scope.digitClick = function (digit) {
+    if ($scope.clearDisplay) {
+        $scope.output = digit;
+        $scope.clearDisplay = false;
+    } else {
+        $scope.output += String(digit);
+    }
+    $scope.tempNumberB = convertToNumber($scope.output);
+  };
+
+  $scope.operatorClick = function (operator) {
+    $scope.currentOperator = operator;
+    $scope.tempNumberA = convertToNumber($scope.output);
+    $scope.clearDisplay = true;
+  };
+
+  $scope.equalsClick = function () {
+    if($scope.currentOperator !== null) {
+      $scope.output = eval($scope.tempNumberA + $scope.currentOperator + $scope.tempNumberB);
+      $scope.clearDisplay = true;
+      $scope.tempNumberA = $scope.output;
+    }
+  };
+
+  convertToNumber = function (string) {
+    return string * 1;
+  }
+});
